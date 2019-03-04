d7581fd0ec65307231b9713673cac773528025de
Nick Pang <nick95927@gmail.com>
2019-03-04 14:04:40 +1000

    Updated more C# like API for Card

--- GarupaPico/GarupaPico/Model/Card.cs
+++ GarupaPico/GarupaPico/Model/Card.cs
@@ -4,9 +4,7 @@ using System.Text;
 
 namespace GarupaPico.Model
 {
-    class Card
-    {
-        public static int ID = 0;                       // card id for the app.
+    class Card {
         public int Id { get; private set; }             // card id from the game.
         public string CardName { get; private set; }    // card title.
         public string Band { get; private set; }        // card character band.
@@ -16,37 +14,34 @@ namespace GarupaPico.Model
         public int Pow { get; private set; }            // power
         public int Tech { get; private set; }           // techique
         public int Vis { get; private set; }            // visual
-        public int Total { get; private set; }          // above 3 sum up.
+        public int Total => Pow + Tech + Vis;           // above 3 sum up.
         public string SkillType { get; private set; }   // card skill description.
         public int ScorePerc { get; private set; }      // percentage of score up
         public float SkillLastS { get; private set; }   // skill last at minimum skill level.
         public float SkillLastL { get; private set; }   // skill last at maximum skill level.
         public string ImgPath { get; private set; }     // online image path
-        // default constructor
-        public Card(string CardName, string Band, string CharaName, 
-            int Rarity, string Type, int Pow, int Tech, int Vis, string ImgPath, string SkillType)
-        {
+
+        // default dummy constructor
+        public Card() {
             Id = -1;
-            this.CardName = CardName;
-            this.Band = Band;
-            this.CharaName = CharaName;
-            this.Rarity = Rarity;
-            this.Type = Type;
-            this.Pow = Pow;
-            this.Tech = Tech;
-            this.Vis = Vis;
-            Total = (this.Pow + this.Tech + this.Vis);
-            //this.ImgPath = "" + this.getResource("/cardIcons/card0.png");
-            this.SkillType = SkillType;
+            CardName = "<なし>";
+            Band = "N/A";
+            CharaName = "N/A";
+            Rarity = 0;
+            Type = "N/A";
+            Pow = 0;
+            Tech = 0;
+            Vis = 0;
+            //ImgPath = "" + this.getResource("/cardIcons/card0.png");
+            this.SkillType = "N/A";
             ScorePerc = 0;
             SkillLastS = 0.0f;
             SkillLastL = 0.0f;
-            ID++;
         }
+
         // full data contrustor
         public Card(int Id, string CardName, string Band, string CharaName, 
-            int Rarity, string Type, int Pow, int Tech, int Vis, string SkillType, int ScorePerc, float SkillLastL, string ImgPath)
-        {
+            int Rarity, string Type, int Pow, int Tech, int Vis, string SkillType, int ScorePerc, float SkillLastL, string ImgPath) {
             this.Id = Id;
             this.CardName = CardName;
             this.Band = Band;
@@ -56,16 +51,14 @@ namespace GarupaPico.Model
             this.Pow = Pow;
             this.Tech = Tech;
             this.Vis = Vis;
-            Total = (this.Pow + this.Tech + this.Vis);
             this.SkillType = SkillType;
             this.ScorePerc = ScorePerc;
             SkillLastS = 5.0f;
             this.SkillLastL = SkillLastL;
             this.ImgPath = ("https://i.imgur.com/" + ImgPath);
-            ID++;
         }
-        public string GetBand()
-        {
+
+        public string GetBand() {
             if (Band.Equals("Poppin'Party", StringComparison.OrdinalIgnoreCase))
                 return "Poppin' Party";
             return Band;
