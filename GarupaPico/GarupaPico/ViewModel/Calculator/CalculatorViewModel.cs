91dec9c32a3b2862d1a52ea4e591109879a47a29
Eric Lam <ericlamyauping@gmail.com>
2019-03-07 22:37:47 +1000

    Bind view model to calculator page

---
+++ GarupaPico/GarupaPico/ViewModel/Calculator/CalculatorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarupaPico.ViewModel.Calculator
+{
+    public class CalculatorViewModel : PageViewModelBase
+    {
+        public CalculatorViewModel()
+        {
+            Title = "Deck";
+        }
+    }
+}
---
+++ GarupaPico/GarupaPico/ViewModel/PageViewModelBase.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows.Input;
+
+namespace GarupaPico.ViewModel
+{
+    public abstract class PageViewModelBase : BindableBase
+    {
+        private string _title;
+        private string _titleIcon;
+        private ICommand _appBarLeftButtonCommand;
+        private ICommand _appBarMiddleButtonCommand;
+        private ICommand _appBarRightButtonCommand;
+
+        /// <summary>
+        /// Gets or sets the page title
+        /// </summary>
+        public string Title
+        {
+            get => _title;
+            protected set
+            {
+                if (_title == value) return;
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the page icon
+        /// </summary>
+        public string TitleIcon
+        {
+            get => _titleIcon;
+            protected set
+            {
+                if (_titleIcon == value) return;
+                _titleIcon = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the action of the left command bar button
+        /// </summary>
+        public ICommand AppBarLeftButtonCommand
+        {
+            get => _appBarLeftButtonCommand;
+            protected set
+            {
+                if (_appBarLeftButtonCommand == value) return;
+                _appBarLeftButtonCommand = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the action of the right command bar button
+        /// </summary>
+        public ICommand AppBarRightButtonCommand
+        {
+            get => _appBarRightButtonCommand;
+            protected set
+            {
+                if (_appBarRightButtonCommand == value) return;
+                _appBarRightButtonCommand = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
